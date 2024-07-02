d47c3ff0da6c672021f9ea1a21d278636465fe98
Cyclone <76272465+Ashisaprogrammer@users.noreply.github.com>
2024-07-02 20:29:00 +0530

    Fixed the Problem with database creation
    Updated the function which creates the database, using a with statement in reference to the Flask app

--- RealEstate-/website/__init__.py
+++ RealEstate-/website/__init__.py
@@ -8,7 +8,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'blahblahblah'
-    app.config['SQLALCHEMY_DATABASE_URL']= f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{DB_NAME}'
     db.init_app(app)  
 
 
@@ -19,12 +19,15 @@ def create_app():
     app.register_blueprint(auth,url_prefix='/')
 
     from .models import User,Note
+    
+    with app.app_context():
+        db.create_all()
 
     return app
 
-def create_database(app):
-    if not path('website/'+DB_NAME):
-        db.create_all(app=app)
-        print('Create Database!')
+#def create_database(app):
+    #if not path.exists('website/'+DB_NAME):
+        #db.create_all(app=app)
+        #print('Created Database!')
         
 
